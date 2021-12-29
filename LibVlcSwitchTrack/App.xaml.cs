d91f715bd56661f462ee949d7dff0a23f2847782
Asteron1024 <asteron1024@gmail.com>
2021-12-29 15:25:50 +0200

    initial

---
+++ LibVlcSwitchTrack/App.xaml.cs
@@ -0,0 +1,8 @@
+﻿using System.Windows;
+
+namespace LibVlcSwitchTrack
+{
+    public partial class App : Application
+    {
+    }
+}
---
+++ LibVlcSwitchTrack/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using LibVLCSharp.Shared;
+using System;
+using System.Windows;
+using MediaPlayer = LibVLCSharp.Shared.MediaPlayer;
+
+namespace LibVlcSwitchTrack
+{
+
+    public partial class MainWindow : Window
+    {
+        private LibVLC _libVLC;
+        private MediaPlayer _mediaPlayer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Core.Initialize();
+
+            _libVLC = new LibVLC();
+            _mediaPlayer = new MediaPlayer(_libVLC);
+
+            VideoView.Loaded += (sender, e) => VideoView.MediaPlayer = _mediaPlayer;
+        }
+
+        private void PlayButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!VideoView.MediaPlayer.IsPlaying)
+            {
+                var uri = new Uri("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4");
+                using (var media = new Media(_libVLC, uri))
+                    VideoView.MediaPlayer.Play(media);
+            }
+        }
+
+        private void Audio0_Click(object sender, RoutedEventArgs e)
+        {
+            VideoView.MediaPlayer.SetAudioTrack(0);
+        }
+
+        private void Audio1_Click(object sender, RoutedEventArgs e)
+        {
+            VideoView.MediaPlayer.SetAudioTrack(-1);
+        }
+    }
+}
